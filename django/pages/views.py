d345bd9314f9a74797a69bf783dc67422b67b398
NaGyeong Park <97649363+NaGyeong-Park@users.noreply.github.com>
2022-03-08 22:27:54 +0900

    Delete django/pages directory

--- django/pages/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def dinner(request, menu, people):
-    content = {'menu' : menu, 'people' : people}
-    return render(request, 'dinner.html', content)
