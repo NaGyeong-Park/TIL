01b0ca78157c2af739d57c56c4645fc29dbc79d8
NaGyeongPark <ngp.july@gmail.com>
2022-06-28 23:16:26 +0900

    simple todos

---
+++ React/react-for-beginner/m-app/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, idx) => (
+        <li key={idx}>{item}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
