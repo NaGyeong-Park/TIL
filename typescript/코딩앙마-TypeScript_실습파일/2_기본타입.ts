74e9bdeeb65e130f823236298ec3c2fc7d5b68fa
NaGyeong Park <ngp.july@gmail.com>
2022-09-30 12:00:00 +0900

    typescript:기본 타입

--- typescript/코딩앙마-TypeScript_실습파일/2_기본타입.ts
+++ typescript/코딩앙마-TypeScript_실습파일/2_기본타입.ts
@@ -1,13 +1,13 @@
 let age: number = 30;
 let isAdult: boolean = true;
-let a: number[] = [1, 2, 3, 4, 5];
+let a1: number[] = [1, 2, 3, 4, 5];
 let a2: Array<number> = [1, 2, 3, 4, 5];
 let week1: string[] = ["mon", "tue", "wed"];
 let week2: Array<string> = ["mon", "tue", "wed"];
 
 // 튜플 (Tuple)
-let b: [string, number];
-b = ["z", 1];
+let b1: [string, number];
+b1 = ["z", 1];
 // b = [1, "z"]; // 에러
 
 // void, never
@@ -25,7 +25,15 @@ function infLoop(): never {
 
 // enum
 enum Os {
-  Window,
+  // 인덱스 값 지정 안하면 자동으로 0 할당됨
+  Window = 3,
   Ios,
   Android,
 }
+// 특정 값만 사용하고 싶을 때, 값들이 공통되어있을 때 사용하자
+let myOs: Os;
+myOs = Os.Window;
+
+// null, undefinde
+let a: null = null;
+let b: undefined = undefined;
