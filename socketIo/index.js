a936736a15c14b32c8b7f29fdad1e40caea3331e
NaGyeong Park <ngp.july@gmail.com>
2022-10-16 12:00:00 +0900

    socket:채팅 예제

--- socketIo/index.js
+++ socketIo/index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    socket.broadcast.emit("hi");
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+  });
 });
 
 server.listen(3000, () => {
